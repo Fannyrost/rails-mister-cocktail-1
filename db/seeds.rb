dcbc4dad1e19e56e7be6d67c7353a11459e153d3
Hugo Fortunier <hfortunier@gmail.com>
2020-08-14 11:06:42 +0200

    push du jeudi

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+Ingredient.destroy_all
+puts "ingredients creation"
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+json = open(url).read
+data = JSON.parse(json)
+drinks = data["drinks"]
+drinks.each do |drink|
+  a = drink["strIngredient1"]
+  Ingredient.create!(name: a)
+end
+puts "ingredient creation done ! I start the cocktails"
+cocktails = ("odkeau, Bloody Mary, Russe blanc, Octobre rouge, Russe noir, Screwdriver, Trou noir, Chien enragé, PPP, Sex on the beach, Blue Lagoon, Cosmopolitan").split(",")
+cocktails.each do |cocktail|
+  Cocktail.create(name: cocktail)
+end
